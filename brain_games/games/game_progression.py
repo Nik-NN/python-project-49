7919dbcfe3565fb18c6c678bd8b2a8c2c3e7fb10
Nikita Nechaev <no655@yandex.ru>
2022-11-14 12:17:49 +0300

    update brain_games

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -4,22 +4,24 @@ from random import randint
 text_game = 'What number is missing in the progression?'
 
 
-def progression():
+def progression(num):
     progression_list = []
     i = 0
-    n = randint(1, 100)
     stride_length = randint(1, 10)
     while i < 10:
-        progression_list.append(1 + n)
+        progression_list.append(1 + num)
         i += 1
-        n += stride_length
-    hidden_element = randint(0, 9)
-    j = progression_list[hidden_element]
-    progression_list[hidden_element] = '..'
-    return [progression_list, j]
+        num += stride_length
+    hidden_element_num = randint(0, 9)
+    hidden_element = progression_list[hidden_element_num]
+    progression_list[hidden_element_num] = '..'
+    progression_list_str = str(progression_list)
+    return [progression_list_str.replace("'", ""), hidden_element]
 
 
 def question_game():
-    [question, answer] = progression()
-    return [str(question)[1:-1], answer]
+    num = randint(1, 100)
+    [question, answer] = progression(num)
+
+    return [question[1:-1], answer]
  
