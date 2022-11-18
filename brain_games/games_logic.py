9641ba79b5cbbb820107fb575df1579bf4a5dc80
Nikita Nechaev <no655@yandex.ru>
2022-11-18 13:13:34 +0300

    fixes brain_games

--- brain_games/games_logic.py
+++ brain_games/games_logic.py
@@ -17,7 +17,8 @@ def start_game(game):
             print('Correct!')
             i += 1
         else:
-            print(f"'{answer}' {text_game_over} '{correct_answer}'")
+            print(f"'{answer}' is wrong answer ;(. Correct answer was "
+            f"{correct_answer}'")
             print(f"Let's try again, {name}!")
             break
     if i == 3:
